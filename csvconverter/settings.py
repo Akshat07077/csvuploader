5d40236059924a1b27d35a9142edfc6c872a7daf
Akshat Sharma <sakshat988@gmail.com>
2024-07-28 20:29:51 +0530

    Update settings.py

--- csvconverter/settings.py
+++ csvconverter/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+DATABASE_URL='postgresql://postgres:eohmbuDBGMVyudcKcKToyXzzyojnQzib@roundhouse.proxy.rlwy.net:48003/railway'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,12 +79,9 @@ WSGI_APPLICATION = 'csvconverter.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
 }
-DATABASES['default'] = dj_database_url.config()
+
 
 
 # Password validation
