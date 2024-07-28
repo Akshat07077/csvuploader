af89ddf363d8e9929d4c08e7094a1287114dcf86
Akshat Sharma <sakshat988@gmail.com>
2024-07-28 19:29:02 +0530

    Update settings.py

--- csvconverter/settings.py
+++ csvconverter/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +83,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
